762144c1f218b7d4269377a3c84250a5b766e59f
andrea-munroe <80475375+andrea-munroe@users.noreply.github.com>
2022-04-11 13:13:04 -0400

    Added new table classes
    Trying to get closer to 3NF

--- createDB/create.sql
+++ createDB/create.sql
@@ -2,11 +2,26 @@ DROP TABLE IF EXISTS answers;
 DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS scores;
 
+
+CREATE TABLE 
+    question_answers (
+        quest_id SERIAL FOREIGN KEY REFERENCES questions (quest_id),
+        answer_text text,
+    );
+    
+ 
+
 CREATE TABLE
     questions (
-        question text PRIMARY KEY NOT NULL,
-        correct_answer text NOT NULL
+        quest_id SERIAL,
+        quest_text text PRIMARY KEY NOT NULL,
     );
+
+CREATE TABLE 
+    test_questions (
+        test_id SERIAL,
+        quest_id SERIAL,
+     );
 
 CREATE TABLE
     answers (
@@ -19,13 +34,12 @@ CREATE TABLE
     test (
         test_id SERIAL,
         test_name VARCHAR(255)
-        question text
-        FOREIGN KEY (question) REFERENCES questions(question) ON DELETE CASCADE
     )
 
 
 CREATE TABLE
     scores (
+        test_id SERIAL,
         student_name VARCHAR(50) NOT NULL,
         score INT NOT NULL CHECK (score <= 100 AND score >= 0)
     );
