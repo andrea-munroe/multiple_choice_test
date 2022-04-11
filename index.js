4d703bfde61d821c6f0b8547b432b5c560defe47
davidbdeath <social_davidbdyer@icloud.com>
2022-04-11 12:01:56 -0400

    updates

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ if (process.env.NODE_ENV !== "production") {
 	require('dotenv').config();
 };
 
-// sql module imports
-// const { Pool } = require('pg')
-// const format = require('pg-format');
-
-// const pool = new Pool()
-
 const querySql = require('./sql/querySql');
 
 // express and ejs module imports
@@ -46,11 +40,34 @@ app.get('/test', (req, res) => {
 	})
 })
 
+app.get('/edit', (req, res) => {
+	const combinedSQL = 'SELECT question, correct_answer, answers FROM questions JOIN answers ON answers.q_id = questions.question';
+	const questions = [];
+
+	querySql(combinedSQL, (query) => {
+		let previous = '';
+		let i = 0;
+		query.forEach((elm) => {
+			if (elm.question !== previous) {
+				previous = elm.question
+				questions.push({ question: elm.question, correct_answer: elm.correct_answer, answers: [] });
+				i++
+			}
+			questions[i - 1].answers.push(elm.answers)
+		})
+		// console.log(questions)
+		res.render('edit', { questions: questions })
+	})
+})
+
+
 app.all('*', (req, res) => {
 	res.render('index')
 });
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Test running on port ${port}`);
+	console.log('http://localhost:3000')
 });
