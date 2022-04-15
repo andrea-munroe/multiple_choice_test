8cc5e6a8856e127948d960b3b61d2e26b6497952
davidbdeath <social_davidbdyer@icloud.com>
2022-04-15 14:43:41 -0400

    updated front end and added testing object

---
+++ example.js
@@ -0,0 +1,29 @@
+module.exports = [{
+    testName: "Marvel History",
+    questions: [{
+        question1: "Who is Spiderman?",
+        answers: ["answer1", "answer2", "answer3"],
+        correct_answer: "answer1",
+      },
+      {
+        question2: "Who is Ant Man?",
+        answers: ["answer1", "answer2", "answer3"],
+        correct_answer: "answer3",
+      }
+    ],
+  },
+  {
+    testName: "DC History",
+    questions: [{
+        question1: "Who is Batman?",
+        answers: ["answer1", "answer2", "answer3"],
+        correct_answer: "answer1",
+      },
+      {
+        question2: "Who is Capt. Marvel?",
+        answers: ["answer1", "answer2", "answer3"],
+        correct_answer: "answer3",
+      }
+    ],
+  }
+];
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // use .env environment variables
 if (process.env.NODE_ENV !== "production") {
-	require('dotenv').config();
+  require('dotenv').config();
 };
 
-const querySql = require('./sql/querySql');
+const example = require('./example');
 
 // express and ejs module imports
 const express = require('express');
@@ -17,57 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, '/views')))
 
 app.get('/', (req, res) => {
-	res.render('index')
+  res.render('index', { example: example })
 })
-
-app.get('/test', (req, res) => {
-	const combinedSQL = 'SELECT question, correct_answer, answers FROM questions JOIN answers ON answers.q_id = questions.question';
-	const questions = [];
-
-	querySql(combinedSQL, (query) => {
-		let previous = '';
-		let i = 0;
-		query.forEach((elm) => {
-			if (elm.question !== previous) {
-				previous = elm.question
-				questions.push({ question: elm.question, correct_answer: elm.correct_answer, answers: [] });
-				i++
-			}
-			questions[i - 1].answers.push(elm.answers)
-		})
-		// console.log(questions)
-		res.render('test', { questions: questions })
-	})
-})
-
-app.get('/edit', (req, res) => {
-	const combinedSQL = 'SELECT question, correct_answer, answers FROM questions JOIN answers ON answers.q_id = questions.question';
-	const questions = [];
-
-	querySql(combinedSQL, (query) => {
-		let previous = '';
-		let i = 0;
-		query.forEach((elm) => {
-			if (elm.question !== previous) {
-				previous = elm.question
-				questions.push({ question: elm.question, correct_answer: elm.correct_answer, answers: [] });
-				i++
-			}
-			questions[i - 1].answers.push(elm.answers)
-		})
-		// console.log(questions)
-		res.render('edit', { questions: questions })
-	})
-})
-
 
 app.all('*', (req, res) => {
-	res.render('index')
+  res.render('index')
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Test running on port ${port}`);
-	console.log('http://localhost:3000')
+  console.log(`Test running on port ${port}`);
+  console.log('http://localhost:3000')
 });
