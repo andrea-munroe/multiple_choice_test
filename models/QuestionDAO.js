8552af03006123b8e89cb5747efec037dd6ad4a4
ChrisHemstock <chris.hemstock@myemail.indwes.edu>
2022-04-15 19:31:42 -0400

    Made more small changes to the DAOs

--- models/QuestionDAO.js
+++ models/QuestionDAO.js
@@ -1,9 +1,10 @@
-// loads connection data from .env file
-if (process.env.NODE_ENV !== "production") {
-	require('dotenv').config();
-};
 class QuestionDAO {
     constructor() {
+        // loads connection data from .env file
+        if (process.env.NODE_ENV !== "production") {
+            require('dotenv').config();
+        };
+
         const { Client } = require('pg');
         const client = new Client();
     }
