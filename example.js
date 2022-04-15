6d54626b6835d1147b1d1723206eec10dd3a1157
davidbdeath <social_davidbdyer@icloud.com>
2022-04-15 18:10:48 -0400

    visual formatting

--- example.js
+++ example.js
@@ -1,19 +1,25 @@
-module.exports = [{
-    testName: "Marvel History",
+module.exports = {
+  "Marvel Trivia": {
+    testName: "Marvel Trivia",
     questions: [{
         question: "Who is Spiderman?",
-        answers: ["answer1", "answer2", "answer3"],
-        correct_answer: "answer1",
+        answers: ["Peter Parker", "Kevin Smith", "Monty Python"],
+        correct_answer: "Peter Parker",
       },
       {
         question: "Who is Ant Man?",
-        answers: ["answer1", "answer2", "answer3"],
-        correct_answer: "answer3",
+        answers: ["Hank Pym", "Fred Durst", "Voltron"],
+        correct_answer: "Hank Pym",
+      },
+      {
+        question: "Why is Wolverine grouchy?",
+        answers: ["Daddy issues", "Always picked last", "All his girl friends die"],
+        correct_answer: "Hank Pym",
       }
     ],
   },
-  {
-    testName: "DC History",
+  "DC Trivia": {
+    testName: "DC Trivia",
     questions: [{
         question: "Who is Batman?",
         answers: ["answer1", "answer2", "answer3"],
@@ -26,4 +32,34 @@ module.exports = [{
       }
     ],
   }
-];
+};
+
+// module.exports = [{
+//   testName: "Marvel History",
+//   questions: [{
+//       question: "Who is Spiderman?",
+//       answers: ["answer1", "answer2", "answer3"],
+//       correct_answer: "answer1",
+//     },
+//     {
+//       question: "Who is Ant Man?",
+//       answers: ["answer1", "answer2", "answer3"],
+//       correct_answer: "answer3",
+//     }
+//   ],
+// },
+// {
+//   testName: "DC History",
+//   questions: [{
+//       question: "Who is Batman?",
+//       answers: ["answer1", "answer2", "answer3"],
+//       correct_answer: "answer1",
+//     },
+//     {
+//       question: "Who is Capt. Marvel?",
+//       answers: ["answer1", "answer2", "answer3"],
+//       correct_answer: "answer3",
+//     }
+//   ],
+// }
+// ];
