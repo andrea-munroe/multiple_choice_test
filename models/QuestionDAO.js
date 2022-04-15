8901bcc7211861592fde7ce4c6fa43ae9223c018
Chris Hemstock <chris.hemstock@myemail.indwes.edu>
2022-04-15 12:11:03 -0400

    "Finished" the Answer and Question DAOs

--- models/QuestionDAO.js
+++ models/QuestionDAO.js
@@ -8,35 +8,85 @@ class QuestionDAO {
         const client = new Client();
     }
 
-    getQuestion(question) {
+    getQuestion(id) {
         client.connect();
         //Check these queries because im not entirly sure what is needed
-        client.query('SELECT * from question where question_text = ?', [question], (error, results) =>
+        client.query('SELECT * from question where quest_id = ?', [id], (error, results) =>
         {
             console.log(error ? error.stack : results.rows[0].message)
             client.end();
         })
     }
 
-    //Would this be more useful if we grabbed all questions from a tests name?
-    getAllQuestions() {
-
+    addQuestion(question) {
+        if(question != "" && question != undefined) {
+            client.connect();
+            //Check these queries because im not entirly sure what is needed
+            client.query('INSERT into question(quest_text, correct_ans) values (?, null)', [question], (error, results) =>
+            {
+                console.log(error ? error.stack : results.rows[0].message)
+                client.end();
+            })
+        } else {
+            console.log("invalid string");
+            //raise exception
+        }
     }
 
-    saveQuestion() {
-
-    }
-
-    updateQuestion() {
-
+    updateQuestion(id, text) {
+        if(text != "" && text != undefined) {
+            client.connect();
+            //Check these queries because im not entirly sure what is needed
+            //not sure about having both [id, text] in the array like that works.
+            client.query('UPDATE question set quest_text = ? where quest_id = ?', [text, id], (error, results) =>
+            {
+                console.log(error ? error.stack : results.rows[0].message)
+                client.end();
+            })
+        } else {
+            console.log("Invalid string");
+            //raise exception
+        }
     }
 
     deleteQuestion(id) {
-
+        client.connect();
+        //Check these queries because im not entirly sure what is needed
+        client.query('DELETE from question where quest_id = ?', [id], (error, results) =>
+        {
+            console.log(error ? error.stack : results.rows[0].message)
+            client.end();
+        })
     }
 
-    deleteQuestion(object) {
-
+    addAnswer(question_id, answer_id) {
+        client.connect();
+        //Check these queries because im not entirly sure what is needed
+        client.query('INSERT into question_answer(quest_id, ans_id) values (?, ?)', [question_id, answer_id], (error, results) =>
+        {
+            console.log(error ? error.stack : results.rows[0].message)
+            client.end();
+        })
     }
 
+    removeAnswer(question_id, answer_id) {
+        client.connect();
+        //Check these queries because im not entirly sure what is needed
+        client.query('DELETE from question_answer where quest_id = ? and ans_id = ?', [question_id, answer_id], (error, results) =>
+        {
+            console.log(error ? error.stack : results.rows[0].message)
+            client.end();
+        })
+    }
+
+    changeCorrectAnswer(question_id, answer_id) {
+        client.connect();
+            //Check these queries because im not entirly sure what is needed
+            //This may create issues if the id given isn't in the question_answer table.
+            client.query('UPDATE question set correct_ans = ? where quest_id = ?', [answer_id, question_id], (error, results) =>
+            {
+                console.log(error ? error.stack : results.rows[0].message)
+                client.end();
+            })
+    }
 }
