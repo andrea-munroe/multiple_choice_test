ec3f1ee35d1be63e4dff6b92e07eeeb0a76e563b
davidbdeath <social_davidbdyer@icloud.com>
2022-04-17 15:56:27 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,17 +1,21 @@
 module.exports = {
   "Marvel Trivia": {
     testName: "Marvel Trivia",
+    test_id: 01,
     questions: [{
+      question_id: 01,
         question: "Who is Spiderman?",
         answers: ["Peter Parker", "Kevin Smith", "Monty Python"],
         correct_answer: "Peter Parker",
       },
       {
+        question_id: 02,
         question: "Who is Ant Man?",
         answers: ["Hank Pym", "Fred Durst", "Voltron"],
         correct_answer: "Hank Pym",
       },
       {
+        question_id: 03,
         question: "Why is Wolverine grouchy?",
         answers: ["Daddy issues", "Always picked last", "All his girl friends die"],
         correct_answer: "Hank Pym",
@@ -20,12 +24,15 @@ module.exports = {
   },
   "DC Trivia": {
     testName: "DC Trivia",
+    test_id: 02,
     questions: [{
+        question_id: 04,
         question: "Who is Batman?",
         answers: ["answer1", "answer2", "answer3"],
         correct_answer: "answer1",
       },
       {
+        question_id: 05,
         question: "Who is Capt. Marvel?",
         answers: ["answer1", "answer2", "answer3"],
         correct_answer: "answer3",
