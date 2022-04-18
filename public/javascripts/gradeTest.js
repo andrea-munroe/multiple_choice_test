6d37d097a91f2420dcdc8d3f6486c903bcad0c39
davidbdeath <social_davidbdyer@icloud.com>
2022-04-18 13:23:00 -0400

    Update gradeTest.js

--- public/javascripts/gradeTest.js
+++ public/javascripts/gradeTest.js
@@ -1,44 +1,48 @@
 const answers = document.querySelectorAll('input[type="radio"]');
 const submitBtn = document.getElementById('submit');
+const submitedAnswers = [];
 
 
 const getAnswers = () => {
-  const submitedAnswers = [];
+  submitedAnswers.length = 0;
   answers.forEach((answer) => {
     if (answer.checked === true) {
       submitedAnswers.push(answer.id);
     }
   });
+  console.log(submitedAnswers)
 };
 
 const markAnswers = () => {
   answers.forEach((answer) => {
     if (answer.checked === true) {
       if (correctAnswers.includes(answer.id) === false) {
-        answer.parentElement.classList.add('text-danger','fw-bold')
+        answer.parentElement.classList.add('text-danger', 'fw-bold')
       } else {
-				answer.parentElement.classList.add('text-success','fw-bold')
-			}
+        answer.parentElement.classList.add('text-success', 'fw-bold')
+      }
     }
   });
 };
 
 const grade = (submited, correct) => {
+  let grade = [0, 0]
+  grade[1] = submited.length
   submited.forEach((sub) => {
     if (correct.includes(sub)) {
-      console.log(sub)
-    } else {
-      console.log(sub.parentElement)
+      grade[0]++
     }
   })
-
+  let score = Math.round(grade[0] / grade[1] * 100)+'%'
+  // console.log(grade[0], grade[1])
+  // console.log(Math.round(grade[0] / grade[1] * 100)+'%')
 }
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   getAnswers();
-	markAnswers();
-  // grade(submitedAnswers, correctAnswers);
+  markAnswers();
+  grade(submitedAnswers, correctAnswers);
   // console.log(submitedAnswers)
 });
 
