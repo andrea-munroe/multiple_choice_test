c28276473456dcfa6d7c2694374fd7217e3991b8
davidbdeath <social_davidbdyer@icloud.com>
2022-04-19 14:32:11 -0400

    updated test sqlquery

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // use .env environment variables
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
+  require('dotenv').config();
 }
 
 // test requires
@@ -18,95 +18,89 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// render home page
-app.get('/', async (req, res) => {
-	const queryString = 'SELECT * FROM test';
-	const { rows } = await db.query(queryString);
+// ------------------------ //
+// ------- Routes --------- //
+// ------------------------ //
 
-	res.render('index', { tests: rows });
+// Home Page
+app.get('/', async (req, res) => {
+  const queryString = 'SELECT * FROM test';
+  const { rows } = await db.query(queryString);
+
+  res.render('index', { tests: rows });
 });
 
-// render test page
-// app.get('/test/:testName', (req, res) => {
-//   const { testName } = req.params;
-//   const correctAnswers = [];
-
-//   example[testName].questions.forEach(element => {
-//     correctAnswers.push(element.correct_answer.split(' ').join('_').toLowerCase())
-//   });
-
-//   res.render('test', { example: example, testName: testName, correctAnswers: correctAnswers })
-// })
+// Test Page
 
 app.get('/test/:testId', async (req, res) => {
-	const { testId } = req.params;
+  const { testId } = req.params;
 
-	const queryString =
-		'SELECT * FROM question	JOIN question_answer ON question.quest_id = question_answer.quest_id JOIN answer ON question_answer.ans_id = answer.ans_id;';
-	const { rows } = await db.query(queryString);
+  // const queryString =
+  //   'SELECT * FROM question	JOIN question_answer ON question.quest_id = question_answer.quest_id JOIN answer ON question_answer.ans_id = answer.ans_id;';
+  const queryString =
+    'SELECT * FROM test natural join test_question natural join question natural join question_answer natural join answer Where test_id = $1';
+  const { rows } = await db.query(queryString, [testId]);
 
-	const questNames = [];
+  const questNames = [];
 
-	// extracts question names from query and pushes them to questNames array
-	rows.forEach((row) => {
-		const tempArray = [];
-		tempArray.push(row.quest_text);
-		tempArray.forEach((element) => {
-			if (!questNames.includes(element)) {
-				questNames.push(element);
-			}
-		});
-	});
+  // extracts question names from query and pushes them to questNames array
+  rows.forEach((row) => {
+    const tempArray = [];
+    tempArray.push(row.quest_text);
+    tempArray.forEach((element) => {
+      if (!questNames.includes(element)) {
+        questNames.push(element);
+      }
+    });
+  });
 
-	// creates an array of objects Question, Answer, Correct Answer
-	questWithAnswers = [];
-	questNames.forEach((name) => {
-		// questWithAnswers[name] = []
-		questWithAnswers[name] = {answers: [], correct:''}
-	});
+  // creates an array of objects Question, Answer, Correct Answer
+  const questWithAnswers = [];
+  questNames.forEach((name) => {
+    questWithAnswers[name] = { answers: [], correct: '' };
+  });
 
-	rows.forEach((row)=>{
-		if(row.quest_text.includes(questWithAnswers)) {
-			questWithAnswers[row.quest_text].answers.push(row.ans_text)
-			questWithAnswers[row.quest_text].correct = row.correct_ans
-		}
-	})
-
-	console.log(questWithAnswers);
-
-	res.render('index', { tests: rows });
+  rows.forEach((row) => {
+    if (row.quest_text.includes(questWithAnswers)) {
+      questWithAnswers[row.quest_text].answers.push(row.ans_text);
+      questWithAnswers[row.quest_text].correct = row.correct_ans;
+    }
+  });
+  console.log(questWithAnswers);
+  res.render('test', { testContent: questWithAnswers, testName: rows[0].test_name })
+  // res.render('index', { tests: rows });
 });
 
 // render test edit page
 app.get('/edit_test/:testName', (req, res) => {
-	const { testName } = req.params;
-	res.render('edit', { example: example, testName: testName });
+  const { testName } = req.params;
+  res.render('edit', { example: example, testName: testName });
 });
 
 // render score display page
 app.get('/score', (req, res) => {
-	res.render('index', { example: example });
+  res.render('index', { example: example });
 });
 
 // insert score into db from test page
 app.post('/scoreSubmit', async (req, res) => {
-	const { scoreDisplay, name } = req.body;
-	let score = scoreDisplay.slice(0, -1);
+  const { scoreDisplay, name } = req.body;
+  let score = scoreDisplay.slice(0, -1);
 
-	const queryString = 'INSERT INTO score VALUES(1, $1, $2)';
-	const { rows } = await db.query(queryString, [name, score]);
+  const queryString = 'INSERT INTO score VALUES(1, $1, $2)';
+  const { rows } = await db.query(queryString, [name, score]);
 
-	res.render('index', { example: example });
+  res.render('index', { example: example });
 });
 
 // catch all render index
 app.all('*', (req, res) => {
-	res.render('index', { example: example });
+  res.render('index', { example: example });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Test running on port ${port}`);
-	console.log('http://localhost:3000');
-	let url = 'http://localhost:3000';
+  console.log(`Test running on port ${port}`);
+  console.log('http://localhost:3000');
+  let url = 'http://localhost:3000';
 });
