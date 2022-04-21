5fc1aba5fa57318ed4ed1bbecbc564679988595b
davidbdeath <social_davidbdyer@icloud.com>
2022-04-21 18:10:53 -0400

    Update DefaultDAO.js

--- models/DefaultDAO.js
+++ models/DefaultDAO.js
@@ -1,11 +1,11 @@
-// require('dotenv').config({ path: '../.env' });
-
-
+const { Pool } = require('pg');
+require('dotenv').config({ path: '../.env' });
+pool = new Pool();
 class DefaultDAO {
 	constructor() {
-		const { Pool } = require('pg');
-		require('dotenv').config({ path: '../.env' });
-		this.pool = new Pool();
+		// const { Pool } = require('pg');
+		// require('dotenv').config({ path: '../.env' });
+		// this.pool = new Pool();
 
 		
 		 
@@ -15,7 +15,7 @@ class DefaultDAO {
 		const query = async() => {
 			const queryString1 = 'Select ans_text from answer where ans_id = $1'
 			//const queryString1 = 'SELECT * FROM test natural join test_question natural join question natural join question_answer natural join answer Where test_id = $1;';
-			const { rows: test } =  await this.pool.query(queryString1, [id]);
+			const { rows: test } =  await pool.query(queryString1, [id]);
 			callback(test)
 		}
 		query();
