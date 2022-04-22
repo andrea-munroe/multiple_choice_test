450d673736d49388f5ea6cbc011bd542bd0328a0
Chris Hemstock <chris.hemstock@myemail.indwes.edu>
2022-04-21 20:11:26 -0400

    Finished questionDAO

--- models/QuestionDAO.js
+++ models/QuestionDAO.js
@@ -23,135 +23,85 @@ class QuestionDAO {
     }
 
     getAllQuestions(test_id, callback) {
-        
         const query = async() => {
             const questions = []
             const sql = 'SELECT quest_id, quest_text, correct_ans from question natural join test_question where test_id = $1'
             const { rows: quest } = await this.pool.query(sql, [test_id])
             //console.log(quest)
             quest.forEach((elm) => {
-                    //console.log("elm", elm)
-                    //console.log(elm.quest_id)
-                    this.ansDAO.getAllAnswers(elm.quest_id, (answers) => {
-                        //console.log(elm)
-                        questions.push(new Question(elm.quest_id, elm.quest_text, answers, elm.correct_ans));
-                        if(quest[quest.length-1] == elm && questions.length == quest.length) {
-                            callback(questions)
-                        }
-                        
-                })
-                
+                this.getQuestion(elm.quest_id, (question) => {
+                    questions.push(question)
+                    if(questions.length == quest.length) {
+                        callback(questions)
+                    }
+                }) 
             })
-            
-            //console.log(ans)
         }
         query()
     }
 
     addQuestion(text, callback) {
-        if(text != "" && text != undefined) {
-            this.client.connect();
-            this.client.query('INSERT into question(quest_text, correct_ans) values (?, null) returning quest_id', [text], (error, results) =>
-            {
-                if(error) {
-                    this.client.end();
-                    throw error;
-                }
-                this.client.end();
-                callback(new Question(results.rows[0].quest_id, text, null, null))
-            })
-        } else {
-            console.log("invalid string");
-            //raise exception
+        const query = async() => {
+            const sql = 'INSERT into question(quest_text, correct_ans) values ($1, null) returning quest_id'
+            const { rows:quest } = await this.pool.query(sql, [text])
+            callback(new Question(quest[0].quest_id, text, null, null))
         }
+        query()
     }
 
-    updateQuestionText(question, text) {
-        if(text != "" && text != undefined) {
-            this.client.connect();
-            this.client.query('UPDATE question set quest_text = ? where quest_id = ?', [text, id], (error) =>
-            {
-                if(error) {
-                    this.client.end();
-                    throw error;
-                }
-                this.client.end();
-                question.quest_text = text;
-            })
-        } else {
-            console.log("Invalid string");
-            //raise exception
+    updateQuestion(question) {
+        const query = async() => {
+            const sql = 'UPDATE question set quest_text = $1, correct_ans = $2 where quest_id = $3'
+            const sql2 = 'DELETE from question_answer where quest_id = $1'
+            const sql3 = 'INSERT into question_answer(quest_id, ans_id) values ($1, $2)'
+            const { rows:quest } = await this.pool.query(sql, [question.quest_text, question.correct_ans, question.quest_id])
+            const { rows:quest2 } = await this.pool.query(sql2, [question.quest_id])
+            if(question.answers != null) {
+                question.answers.forEach(async(elm) => {
+                    const { rows:quest3 } = await this.pool.query(sql3, [question.quest_id, elm.ans_id])
+                    console.log(question)
+                })
+            }
+            
         }
+        query()
     }
 
-    deleteQuestion(question) {
-        this.client.connect();
-        this.client.query('DELETE from question where quest_id = ?', [question.id], (error) =>
-        {
-            if(error) {
-                this.client.end();
-                throw error;
-            }
-            this.client.end();
-        })
-    }
-
-    //move to answerDAO
-    addAnswer(question, answer) {
-        this.client.connect();
-        this.client.query('INSERT into question_answer(quest_id, ans_id) values (?, ?)', [question.id, answer.id], (error) =>
-        {
-            if(error) {
-                this.client.end();
-                throw error;
-            }
-            this.client.end();
-            question.answers.push(answer)
-        })
-    }
-
-    //move to answerDAO
-    removeAnswer(question, answer) {
-        this.client.connect();
-        this.client.query('DELETE from question_answer where quest_id = ? and ans_id = ?', [question.id, answer.id], (error) =>
-        {
-            if(error) {
-                this.client.end();
-                throw error;
-            }
-            this.client.end();
-            for(let i = 0; i < question.answers.length; i++) {
-                if(question.answers[i].id == answer.id) {
-                    question.answers.splice()
-                }
-            }
-        })
-    }
-
-    setCorrectAnswer(question, answer) {
-        this.client.connect();
-        this.client.query('UPDATE question set correct_ans = ? where quest_id = ?', [answer.id, question.id], (error) =>
-        {
-            if(error) {
-                this.client.end();
-                throw error;
-            }
-            this.client.end();
-            question.correct_ans = answer.id;
-        })
+    deleteQuestion(quest_id) {
+        const query = async() => {
+            const sql = 'DELETE from question where quest_id = $1'
+            const { rows:quest } = await this.pool.query(sql, [quest_id])
+        }
+        query()
     }
 }
 
 module.exports = QuestionDAO;
 
 
-const dao = new QuestionDAO()
+const qdao = new QuestionDAO()
+const adao = new AnswerDAO()
 
-// dao.getQuestion(1, (question) => {
+// qdao.getQuestion(1, (question) => {
 //     console.log(question)
 //     console.log(question.getCorrectAnswer())
 // })
 
-dao.getAllQuestions(1, (questions) => {
-    console.log(questions[1].answers[1])
-})
+// qdao.getAllQuestions(1, (questions) => {
+//     console.log(questions)
+// })
+
+// qdao.addQuestion("This is a new question. Did it work?", (question) => {
+//     console.log(question)
+//     adao.addAnswer(question.id, "This is an answer to the new question.", (answer) => {
+//         console.log(question)
+//         adao.addAnswer(question.id, "This is the second answer to the new question.", (answer) => {
+//             console.log(question)
+//             qdao.updateQuestion(question)
+//         })
+//     })
+// })
+
+// qdao.getQuestion(9, (question) => {
+//     console.log(question)
+// })
